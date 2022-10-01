9cf20a571bd16989991d3bd5d3f868d8c5840a05
Alexander-Aparicio <alexdel2022@gmail.com>
2022-10-01 10:30:13 -0500

    Blog 500s loader

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,30 +1,29 @@
-import BannerBlog from "../components/blog/Banner"
-import ClasterMenu from "../components/blog/ClasterMenu"
-import { Page } from "../helpers/Elementos"
-import Loading from "../helpers/Loader"
-import { useState } from "react"
-import { BoxClaster } from "../components/blog/ClasterMenuComponents"
+import BannerBlog from "../components/blog/Banner";
+import ClasterMenu from "../components/blog/ClasterMenu";
+import { Page } from "../helpers/Elementos";
+import Loading from "../helpers/Loader";
+import { useState } from "react";
+import { BoxClaster } from "../components/blog/ClasterMenuComponents";
 
-const Blog = ()=>{
+const Blog = () => {
+  const [banner, setBanner] = useState(false);
 
-    const [banner, setBanner] = useState(false)
+  const handleLoader = () => {
+    setTimeout(() => {
+      setBanner(true);
+    }, 500);
+  };
 
-    const handleLoader = ()=>{
-        setTimeout(() => { setBanner(true) }, 450)
-    }
+  return (
+    <Page>
+      <BannerBlog />
+      {handleLoader()}
+      <BoxClaster>
+        {!banner ? <Loading /> : null}
+        {banner ? <ClasterMenu /> : null}
+      </BoxClaster>
+    </Page>
+  );
+};
 
-    return(
-
-        <Page>
-            <BannerBlog />
-            {handleLoader()}
-            <BoxClaster>
-                { !banner ? <Loading/> : null }
-                { banner ? <ClasterMenu /> : null}
-            </BoxClaster>
-        </Page>
-
-    )
-}
-
-export default Blog
+export default Blog;
